9494b6e09de376d5246a72d1106834e1ed6dd454
Claudio Rivera <claudio@foundrymakes.com>
2023-09-15 16:24:18 -0500

    🐛 Fix input component

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,24 +1,23 @@
 "use client";
 
-import { type ComponentPropsWithoutRef } from "react";
+import { forwardRef, type ComponentProps } from "react";
 import { Input as _Input } from "~/components/ui/input";
 import { Label } from "~/components/ui/label";
 
-export const Input = ({
-	label,
-	errorMessage,
-	...inputProps
-}: {
-	label: string;
-	errorMessage?: string;
-} & ComponentPropsWithoutRef<typeof _Input>) => {
+export const Input = forwardRef<
+	HTMLInputElement,
+	ComponentProps<typeof _Input> & {
+		label?: string;
+		errorMessage?: string;
+	}
+>(function Input({ label, errorMessage, ...inputProps }, ref) {
 	return (
 		<div className="flex items-center gap-2">
 			<Label className="flex flex-1 flex-col gap-1">
 				{label}
-				<_Input {...inputProps} />
+				<_Input ref={ref} {...inputProps} />
 				{errorMessage && <p className="text-xs text-red-500">{errorMessage}</p>}
 			</Label>
 		</div>
 	);
-};
+});
