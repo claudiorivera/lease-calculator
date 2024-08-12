a65bc5a6c23f83d7c6ce8c5303de13e742eaefa1
Claudio Rivera <me@claudiorivera.com>
2024-08-11 20:33:48 -0500

    ✨ Use recharts

--- src/app/leases/[leaseId]/lease-chart.tsx
+++ src/app/leases/[leaseId]/lease-chart.tsx
@@ -1,7 +1,15 @@
 "use client";
 
-import { AgChartsReact } from "ag-charts-react";
-import { useTheme } from "next-themes";
+import {
+	Area,
+	CartesianGrid,
+	ComposedChart,
+	Label,
+	Line,
+	XAxis,
+	YAxis,
+} from "recharts";
+import { type ChartConfig, ChartContainer } from "~/components/ui/chart";
 import { getLeaseChartData } from "~/lib/leases";
 import type { LeaseByIdOutput } from "~/server/api/routers/lease";
 
@@ -14,102 +22,46 @@ export function LeaseChart({
 		lease,
 	});
 
-	const { resolvedTheme } = useTheme();
-	const isDarkMode = resolvedTheme === "dark";
-
-	const DEFAULT_COLOR = "gray";
-	const BG_NEUTRAL = isDarkMode ? "#262626" : "#e5e5e5";
+	const chartConfig = {
+		odometerReading: {
+			label: "Miles",
+		},
+		expectedOdometerReading: {
+			label: "Expected Miles",
+		},
+	} satisfies ChartConfig;
 
 	return (
-		<section className="w-full aspect-square">
-			<AgChartsReact
-				options={{
-					theme: {
-						baseTheme: isDarkMode ? "ag-default-dark" : "ag-default",
-						palette: {
-							fills: isDarkMode
-								? [DEFAULT_COLOR, "white"]
-								: [DEFAULT_COLOR, "black"],
-							strokes: [DEFAULT_COLOR],
-						},
-						overrides: {
-							common: {
-								background: {
-									fill: BG_NEUTRAL,
-								},
-							},
-						},
-					},
-					legend: {
-						enabled: false,
-					},
-					axes: [
-						{
-							type: "number",
-							position: "left",
-							min: lease.initialMiles,
-							max: lease.allowedMiles + lease.initialMiles,
-							nice: false,
-							tick: {
-								interval: lease.allowedMiles / 6,
-								color: BG_NEUTRAL,
-							},
-							label: {
-								formatter: ({ index, value }) => (index === 0 ? "" : value),
-							},
-							gridLine: {
-								style: [
-									{
-										stroke: DEFAULT_COLOR,
-									},
-								],
-							},
-						},
-						{
-							type: "number",
-							position: "bottom",
-							min: 0,
-							max: lease.numberOfMonths,
-							tick: {
-								interval: lease.numberOfMonths / 6,
-								color: BG_NEUTRAL,
-							},
-							label: {
-								formatter: ({ index, value }) => (index === 0 ? "" : value),
-							},
-							gridLine: {
-								style: [
-									{
-										stroke: DEFAULT_COLOR,
-									},
-								],
-							},
-						},
-					],
-					data,
-					series: [
-						{
-							type: "area",
-							xKey: "monthsSinceLeaseStart",
-							yKey: "expectedOdometerReading",
-							tooltip: {
-								enabled: false,
-							},
-							fillOpacity: 0.25,
-							lineDash: [3, 3],
-							strokeWidth: 1,
-						},
-						{
-							type: "line",
-							xKey: "monthsSinceLeaseStart",
-							yKey: "odometerReading",
-							tooltip: {
-								enabled: false,
-							},
-						},
-					],
-				}}
-			/>
+		<section>
+			<ChartContainer
+				config={chartConfig}
+				className="min-h-[375px] w-full aspect-square"
+			>
+				<ComposedChart data={data} accessibilityLayer>
+					<CartesianGrid stroke="gray" />
+					<XAxis type="number" dataKey="monthsSinceLeaseStart" tickLine={false}>
+						<Label position="insideBottom" offset={-2}>
+							Months
+						</Label>
+					</XAxis>
+					<YAxis
+						type="number"
+						dataKey="expectedOdometerReading"
+						tickLine={false}
+					>
+						<Label angle={-90} position="insideLeft" offset={8}>
+							Miles
+						</Label>
+					</YAxis>
+					<Area
+						dataKey="expectedOdometerReading"
+						fill="gray"
+						stroke="white"
+						strokeDasharray="3"
+					/>
+					<Line dataKey="odometerReading" stroke="white" />
+				</ComposedChart>
+			</ChartContainer>
 		</section>
 	);
 }
