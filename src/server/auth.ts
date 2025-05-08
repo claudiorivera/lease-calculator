dc6fd3999902d003a2eca2f5b23eae9985f78375
Claudio Rivera <me@claudiorivera.com>
2025-05-07 21:26:14 -0500

    ⬆️ Bump deps

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -1,9 +1,7 @@
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth, { type NextAuthConfig } from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
-import EmailProvider from "next-auth/providers/email";
 import GitHubProvider from "next-auth/providers/github";
-import { env } from "~/env.mjs";
 import { db } from "~/server/db";
 
 /**
@@ -13,14 +11,7 @@ import { db } from "~/server/db";
  */
 const authOptions = {
 	adapter: PrismaAdapter(db),
-	providers: [
-		GitHubProvider,
-		DiscordProvider,
-		EmailProvider({
-			server: env.EMAIL_SERVER,
-			from: env.EMAIL_FROM,
-		}),
-	],
+	providers: [GitHubProvider, DiscordProvider],
 	callbacks: {
 		session: ({ session, user }) => ({
 			...session,
